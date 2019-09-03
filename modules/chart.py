b2109b70b10fb928f4f84980528bd4b0dd1a907e
kingrayhan <rayhan095@gmail.com>
2019-09-03 10:41:08 +0600

    readme

--- modules/chart.py
+++ modules/chart.py
@@ -5,7 +5,7 @@ def openChart(percentages):
     labels = ['70%', '60%', '45%', '30%']
     sizes = [] + percentages
     colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
-    patches, texts = plt.pie(sizes, colors=colors, shadow=True, startangle=90)
+    patches, texts = plt.pie(sizes, colors=colors, startangle=90)
     plt.legend(patches, labels, loc="best")
     plt.axis('equal')
     plt.tight_layout()
